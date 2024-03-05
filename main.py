265ec4836e346f40e4e817e4a2c15e6e9383a474
Eric Kapalka <eric.kapalka@montana.edu>
2024-03-05 15:05:23 -0700

    Added database object initialization

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from browser_control.axiom_driver import AxiomDriver
-
+from database_sqlite.database import DatabaseManager
 
 if __name__ == '__main__':
-    driver = AxiomDriver()
+    db_uri = "sqlite:///errors.db"
+    db_manager = DatabaseManager(db_uri)
+    driver = AxiomDriver(db_manager)
