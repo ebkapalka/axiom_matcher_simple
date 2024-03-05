aaf4917a6469f4c8fbc7127d5d0d7ebf15340f31
Eric Kapalka <eric.kapalka@montana.edu>
2024-03-05 16:57:41 -0700

    Changed default database URI

--- main.py
+++ main.py
@@ -2,6 +2,6 @@ from browser_control.axiom_driver import AxiomDriver
 from database_sqlite.database import DatabaseManager
 
 if __name__ == '__main__':
-    db_uri = "sqlite:///errors.db"
+    db_uri = "sqlite:///database_sqlite/database.db"
     db_manager = DatabaseManager(db_uri)
     driver = AxiomDriver(db_manager)
