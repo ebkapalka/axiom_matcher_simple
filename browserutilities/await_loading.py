4bfbff7fd8ed2e4fd9bc71c178a7f048b99fb189
Eric Kapalka <eric.kapalka@montana.edu>
2024-03-05 14:09:00 -0700

    Added second check for the big horizontal loading bar and renamed the whole function to wait_for_loading

--- browserutilities/await_loading.py
+++ browserutilities/await_loading.py
@@ -4,6 +4,8 @@ from selenium.webdriver.common.by import By
 from selenium import webdriver
 
 
-def wait_for_spinner(driver: webdriver):
+def wait_for_loading(driver: webdriver):
     WebDriverWait(driver, 30).until(
         EC.invisibility_of_element_located((By.CLASS_NAME, "loader-container")))
+    WebDriverWait(driver, 30).until(
+        EC.invisibility_of_element_located((By.CLASS_NAME, "progress-bar")))
