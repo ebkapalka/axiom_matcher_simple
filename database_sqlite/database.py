4af7d0aa7e89cf108e8615e000510ffd5fec116e
Eric Kapalka <eric.kapalka@montana.edu>
2024-03-05 16:58:22 -0700

    Changed echo from true to false to reduce console clutter

--- database_sqlite/database.py
+++ database_sqlite/database.py
@@ -2,11 +2,11 @@ from sqlalchemy.orm import sessionmaker, Session
 from sqlalchemy import create_engine, func
 import atexit
 
-from models import Base, AxiomEvent
+from database_sqlite.models import Base, AxiomEvent
 
 class DatabaseManager:
     def __init__(self, uri: str):
-        self.engine = create_engine(uri, echo=True)
+        self.engine = create_engine(uri, echo=False)
         Base.metadata.create_all(bind=self.engine)
         self.SessionLocal = sessionmaker(autocommit=False,
                                          autoflush=False,
