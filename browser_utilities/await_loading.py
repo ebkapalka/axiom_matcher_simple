95ce33cb7d7ffd8605617cdf02df09bcb04a04fc
Eric Kapalka <eric.kapalka@montana.edu>
2024-03-06 17:06:50 -0700

    Added WebDriverWaits for two new spinner / loading element types

--- browser_utilities/await_loading.py
+++ browser_utilities/await_loading.py
@@ -4,8 +4,23 @@ from selenium.webdriver.common.by import By
 from selenium import webdriver
 
 
-def wait_for_loading(driver: webdriver):
-    WebDriverWait(driver, 30).until(
+def wait_for_loading(driver: webdriver, timeout=300):
+    """
+    Wait for all loading bars / spinners / icons to disappear
+    :param driver: webdriver
+    :param timeout: int
+    :return: None
+    """
+    WebDriverWait(driver, timeout).until(
         EC.invisibility_of_element_located((By.CLASS_NAME, "loader-container")))
-    WebDriverWait(driver, 30).until(
+    WebDriverWait(driver, timeout).until(
         EC.invisibility_of_element_located((By.CLASS_NAME, "progress-bar")))
+    WebDriverWait(driver, timeout).until(
+        EC.invisibility_of_element_located((By.ID, "fullpage-overlay")))
+    WebDriverWait(driver, timeout).until(
+        lambda d: _class_no_longer_present(d, (By.CSS_SELECTOR, "input.updating")))
+
+
+def _class_no_longer_present(driver: webdriver, selector: tuple):
+    elements = driver.find_elements(*selector)
+    return len(elements) == 0
