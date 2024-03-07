8acd7a6db69b719afa5e9d231c7d580dcddbf0a6
Eric Kapalka <eric.kapalka@montana.edu>
2024-03-07 11:23:36 -0700

    Refactored select_address to take a dict instead of two strings.  This allows the city / state / zip to be used in the decision process, which hasn't been implemented yet but needs to be.

--- matching_utilities/address_picker.py
+++ matching_utilities/address_picker.py
@@ -1,12 +1,12 @@
-def select_address(addr1: str, addr2: str) -> str:
+def select_address(addr_dict: dict) -> str:
     """
     Select the best address from two addresses
     :param addr1: first address
     :param addr2: second address
     :return: ideal address
     """
-    addr2 = addr2.rstrip(". Please review. Address Line 2 is not blank.")
-    addr1_stripped = addr1.lower().strip()
+    addr2 = addr_dict['Address 2'].rstrip(". Please review. Address Line 2 is not blank.")
+    addr1_stripped = addr_dict['Address 1'].lower().strip()
     addr2_stripped = addr2.lower().strip()
     addr1_stripped = addr1_stripped if addr1_stripped != "null" else ""
     addr2_stripped = addr2_stripped if addr2_stripped != "null" else ""
@@ -14,22 +14,22 @@ def select_address(addr1: str, addr2: str) -> str:
     if not addr1_stripped and not addr2_stripped:
         return ''
     elif addr1_stripped == addr2_stripped:
-        return addr1
+        return addr_dict['Address 1']
     else:
         addr1_has_alnum = contains_letters_and_numbers(addr1_stripped)
         addr2_has_alnum = contains_letters_and_numbers(addr2_stripped)
         if not addr1_has_alnum and not addr2_has_alnum:
             return ''
         if addr1_has_alnum and not addr2_has_alnum:
-            return addr1
+            return addr_dict['Address 1']
         elif addr2_has_alnum and not addr1_has_alnum:
             return addr2
         elif addr1_has_alnum and addr2_has_alnum:
             if "box" in addr1_stripped:
-                return addr1
+                return addr_dict['Address 1']
             if "box" in addr2_stripped:
                 return addr2
-        return addr1
+        return addr_dict['Address 1']
         # TODO: add case for those weird Utah addresses, e.g. 1234 N 5678 W
         # TODO: add check for city or state in the address1 field
         # TODO: refactor this function to take a dict with address1, address2, city, state, and zip
