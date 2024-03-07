8e64cede7eb23296ed85af53e83454c0953e3da6
Eric Kapalka <eric.kapalka@montana.edu>
2024-03-07 16:14:45 -0700

    Moved all nested functions from the functions in this file to address_utils.py

--- matching_utilities/address_picker.py
+++ matching_utilities/address_picker.py
@@ -1,5 +1,6 @@
-import re
-
+from matching_utilities.address_utils import (split_into_parts,
+                                              process_addr,
+                                              contains_letters_and_numbers)
 
 # Define cardinal directions and their abbreviations
 DIRECTIONS = {
@@ -26,6 +27,10 @@ def select_address(addr_dict: dict) -> str:
     addr1_stripped = addr1_stripped if addr1_stripped != "null" else ""
     addr2_stripped = addr2_stripped if addr2_stripped != "null" else ""
 
+    city = addr_dict['City'].lower().strip()
+    state = addr_dict['State'].lower().strip()
+    zip_code = addr_dict['Zip'].strip()
+
     if not addr1_stripped and not addr2_stripped:
         return ''
     elif addr1_stripped == addr2_stripped:
@@ -45,64 +50,23 @@ def select_address(addr_dict: dict) -> str:
             if "box" in addr2_stripped:
                 return addr2
         return addr_dict['Address 1']
-        # TODO: add case for those weird Utah addresses, e.g. 1234 N 5678 W
         # TODO: add check for city or state in the address1 field
-        # TODO: refactor this function to take a dict with address1, address2, city, state, and zip
 
 
-def contains_letters_and_numbers(s: str) -> bool:
-    """
-    Check if a string contains both letters and numbers
-    :param s: string to check
-    :return: whether the string contains both letters and numbers
-    """
-    contains_letters = any(c.isalpha() for c in s)
-    contains_numbers = any(c.isdigit() for c in s)
-    return contains_letters and contains_numbers
-
-
-def is_split_utah_address(addr1, addr2):
+def is_split_utah_address(addr1, addr2) -> tuple[str, str]:
     """
     Check if two addresses are actually a split Utah address
     :param addr1: address 1
     :param addr2: address 2
     :return: True if it meets the specific conditions for a split Utah address, False otherwise
     """
-    def _process_initial(addr: str) -> list[str]:
-        """
-        Split the incoming string wherever a number and non-number are adjacent to each other,
-        and then split the individual parts again by non-alphanumeric characters.  This will weed
-        out any regular addresses and leave only return True for potential split Utah addresses.
-        :param addr: address string to process initially
-        :return: list of strings after initial processing
-        """
-        # Split where a number and non-number character are adjacent
-        parts = re.split(r'(?<=\d)(?=\D)|(?<=\D)(?=\d)', addr)
-        processed_parts = []
-        for part in parts:
-            # Further split by non-alphanumeric characters and filter empty strings
-            sub_parts = [sub_part for sub_part in re.split(r'\W+', part) if sub_part]
-            if len(sub_parts) > 2:
-                return []  # Return an empty list to indicate a failure in the condition
-            processed_parts.extend(sub_parts)
-        return processed_parts
+    # Initial processing to check if the address is too complex to be a Utah address
+    if not split_into_parts(addr1) or not split_into_parts(addr2):
+        return addr1, addr2
 
-    def _process_addr(addr: str) -> list[str]:
-        """
-        Step 1, 2, and 3: Lowercase, replace non-alpha chars, and split by spaces
-        :param addr: address string to process
-        :return: list of strings after processing
-        """
-        return [s.strip() for s in ''.join(
-            c if c.isalpha() else ' '
-            for c in addr.lower()).split()]
-
-    # Initial processing to check the new condition
-    if not _process_initial(addr1) or not _process_initial(addr2):
-        return False  # Early return if the new condition fails
-
-    addr1_parts = _process_addr(addr1)
-    addr2_parts = _process_addr(addr2)
+    # try to isolate cardinal directions from the address parts
+    addr1_parts = process_addr(addr1)
+    addr2_parts = process_addr(addr2)
     addr1_dirs = [DIRECTIONS[part] for part in addr1_parts if part in DIRECTIONS]
     addr2_dirs = [DIRECTIONS[part] for part in addr2_parts if part in DIRECTIONS]
 
@@ -110,5 +74,5 @@ def is_split_utah_address(addr1, addr2):
     if (len(addr1_dirs) == 1
             and len(addr2_dirs) == 1
             and set(addr1_dirs) != set(addr2_dirs)):
-        return True
-    return False
+        return f"{addr1} {addr2}", ""
+    return addr1, addr2
