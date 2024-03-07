1421e7754fd5bcfa6f10dfc0378c685c95d640fc
Eric Kapalka <eric.kapalka@montana.edu>
2024-03-06 17:03:04 -0700

    Added a small bit of logic to allow switching between Axiom prod and Axiom test

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from browser_control.axiom_driver import AxiomDriver
 from database_sqlite.database import DatabaseManager
 
 if __name__ == '__main__':
+    run_mode = "test"
     db_uri = "sqlite:///database_sqlite/database.db"
     db_manager = DatabaseManager(db_uri)
-    driver = AxiomDriver(db_manager)
+    driver = AxiomDriver(db_manager, run_mode)
