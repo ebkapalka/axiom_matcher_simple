cb2475e24b6cebf7964d85e99bdee6d58ec4ae3b
Eric Kapalka <eric.kapalka@montana.edu>
2024-03-08 13:58:10 -0700

    Changed "test" to "prod"

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from browser_control.axiom_driver import AxiomDriver
 from database_sqlite.database import DatabaseManager
 
 if __name__ == '__main__':
-    run_mode = "test"
+    run_mode = "prod"
     db_uri = "sqlite:///database_sqlite/database.db"
     db_manager = DatabaseManager(db_uri)
     driver = AxiomDriver(db_manager, run_mode)
