aa6159885643dbbcb03b37e20269253418d1f3cb
Eric Kapalka <eric.kapalka@montana.edu>
2024-03-07 17:10:51 -0700

    Added logic to extract data from the potential match table.  Also added [currently empty] "decide_best_match" function to break that logic out of handle_match_dialogue to make it better comprehensible

--- matching_utilities/match_handler.py
+++ matching_utilities/match_handler.py
@@ -16,15 +16,39 @@ def handle_match_dialogue(driver: webdriver) -> str:
     match_table = WebDriverWait(driver, 10).until(
         EC.presence_of_element_located((By.ID, "MatchListTable")))
     records = match_table.find_elements(By.XPATH, "./tbody/tr")
+    incoming_prospect = {}
+    potential_matches = []
     for record in records[1:]:
         record.click()
         comparison_table = WebDriverWait(driver, 10).until(
             EC.presence_of_element_located((By.ID, "MatchComparisonTable")))
         rows = comparison_table.find_elements(By.XPATH, "./tbody/tr")
-        for row in rows:
-            # TODO: extract the data from the table
-            print(row.text)
-        print()
+
+        # extract the values for the potential match
+        potential_match = {}
+        for row in rows[1:]:
+            potential_match[row.find_element(By.XPATH, "./td[1]").text] = (
+                row.find_element(By.XPATH, "./td[3]").text)
+        potential_matches.append(potential_match)
+
+        # extract the values for the incoming prospect
+        if not incoming_prospect:
+            for row in rows[1:]:
+                incoming_prospect[row.find_element(By.XPATH, "./td[1]").text] = (
+                    row.find_element(By.XPATH, "./td[2]").text)
+    print(incoming_prospect)
+    for potential_match in potential_matches:
+        print(potential_match)
     time.sleep(100000000)
     # TODO: return only "skipped" or "".  Any other return value will
     #  be handled by the normal handler after this dialogue is closed.
+
+
+def decide_best_match(incoming_prospect: dict, potential_matches: list[dict]) -> str:
+    """
+    Decide the best match from a list of potential matches.
+    :param incoming_prospect: Dictionary mapping column headers to values for the incoming prospect.
+    :param potential_matches: List of dictionaries mapping column headers to values for potential matches.
+    :return: Student_id of the best match.
+    """
+    pass
