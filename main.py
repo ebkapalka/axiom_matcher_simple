6b086f8b33fb2ee63a203e6d81ea120a2880cd05
Eric Kapalka <eric.kapalka@montana.edu>
2024-03-13 10:42:12 -0600

    Added flag called "option" to indicate whether to navigate to the default verifier or the error verifier

--- main.py
+++ main.py
@@ -3,6 +3,10 @@ from database_sqlite.database import DatabaseManager
 
 if __name__ == '__main__':
     run_mode = "prod"
+    option = "error"
+    # TODO: add option to pick a different verifier tile
     db_uri = "sqlite:///database_sqlite/database.db"
     db_manager = DatabaseManager(db_uri)
-    driver = AxiomDriver(db_manager, run_mode)
+    driver = AxiomDriver(db_manager,
+                         run_mode,
+                         option)
