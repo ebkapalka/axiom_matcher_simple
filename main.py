9cc5620d5dabfd1b174671e32513cb91e408ad67
Eric Kapalka <eric.kapalka@montana.edu>
2024-03-13 19:41:58 -0600

    Added multiprocessing so that "default" and "error" can be handled concurrently

--- main.py
+++ main.py
@@ -1,12 +1,31 @@
 from browser_control.axiom_driver import AxiomDriver
 from database_sqlite.database import DatabaseManager
 
+from multiprocessing import Process
+
+
+def run_axiom_driver(uri: str, mode: str, opt: str):
+    """
+    Run the AxiomDriver in a separate process
+    :param uri: uri for the database
+    :param mode: "prod" or "test"
+    :param option: "default" or "error"
+    :return: None
+    """
+    db_manager = DatabaseManager(uri)
+    AxiomDriver(db_manager, mode, opt)
+
+
 if __name__ == '__main__':
     run_mode = "prod"
-    option = "error"
-    # TODO: add option to pick a different verifier tile
+    options = ["default", "error"]
     db_uri = "sqlite:///database_sqlite/database.db"
-    db_manager = DatabaseManager(db_uri)
-    driver = AxiomDriver(db_manager,
-                         run_mode,
-                         option)
+    processes = []
+    for option in options:
+        p = Process(target=run_axiom_driver, args=(db_uri, run_mode, option))
+        processes.append(p)
+        p.start()
+
+    # Wait for all processes to complete
+    for p in processes:
+        p.join()
