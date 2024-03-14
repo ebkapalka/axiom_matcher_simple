4b67bd19dbda44749be8a91482c3d005cc9e4841
Eric Kapalka <eric.kapalka@montana.edu>
2024-03-14 17:01:05 -0600

    Removed HANDLERS since this class does none of that.  Also commented out the whole main loop because that needs to be rewritten entirely

--- browser_control/axiom_fetcher.py
+++ browser_control/axiom_fetcher.py
@@ -4,32 +4,20 @@ from selenium.webdriver.common.by import By
 from selenium import webdriver
 import sys
 
-from browser_utilities.case_handlers import (handle_normal, handle_bad_ceeb, handle_bad_address,
-                                             handle_bad_firstname, handle_bad_lastname,
-                                             handle_bad_city, handle_bad_zip, handle_bad_state)
 from browser_utilities.navigate_verifier import identify_page, goto_verifier
 from matching_utilities.match_handler import handle_match_dialogue
 from browser_utilities.await_loading import wait_for_loading
 from database_sqlite.database import DatabaseManager
 
-
-HANDLERS = {
-    "match dialogue": handle_match_dialogue,
-    "normal": handle_normal,
-    "error - Address 1": handle_bad_address,
-    "error - Address 2": handle_bad_address,
-    "error - High School CEEB": handle_bad_ceeb,
-    "error - First Name": handle_bad_firstname,
-    "error - Last Name": handle_bad_lastname,
-    "error - City": handle_bad_city,
-    "error - State": handle_bad_state,
-    "error - Zip": handle_bad_zip,
-    # add other handlers here as needed
+REC_TYPES = {
+    "prospects": 601,
+    "act": 361,
+    # add other record types here as needed
 }
 
-
 class AxiomFetcher:
-    def __init__(self, database: DatabaseManager, run_mode: str = "test", option: str = "default"):
+    def __init__(self, database: DatabaseManager, config: dict, credentials: dict):
+        run_mode = config["environment mode"]
         if run_mode == "test":
             self.base_url = "https://axiom-elite-test.msu.montana.edu/"
         elif run_mode == "prod":
@@ -40,7 +28,7 @@ class AxiomFetcher:
 
         self.driver = webdriver.Chrome()
         self.database = database
-        self.option = option
+        self.option = config["issue type"]
         self.await_login()
         goto_verifier(self.driver,
                       self.base_url,
@@ -62,19 +50,20 @@ class AxiomFetcher:
         Main logic loop
         :return: None
         """
-        while True:
-            wait_for_loading(self.driver)
-            WebDriverWait(self.driver, 30).until(
-                EC.visibility_of_element_located((By.ID, "VerifyRecordViewPort")))
-            page = identify_page(self.driver)
-            if page == "finished":
-                return
-            elif page in HANDLERS:
-                result = HANDLERS[page](self.driver)
-                if page.startswith("error"):
-                    self.database.add_event(page)
-                if result:
-                    self.database.add_event(result)
-            else:
-                print(f"Unknown page: {page}")
-                input("Press Enter to continue")
+        ...
+        # while True:
+        #     wait_for_loading(self.driver)
+        #     WebDriverWait(self.driver, 30).until(
+        #         EC.visibility_of_element_located((By.ID, "VerifyRecordViewPort")))
+        #     page = identify_page(self.driver)
+        #     if page == "finished":
+        #         return
+        #     elif page in HANDLERS:
+        #         result = HANDLERS[page](self.driver)
+        #         if page.startswith("error"):
+        #             self.database.add_event(page)
+        #         if result:
+        #             self.database.add_event(result)
+        #     else:
+        #         print(f"Unknown page: {page}")
+        #         input("Press Enter to continue")
