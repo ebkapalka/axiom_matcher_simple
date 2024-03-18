6cc3733e58e03aa5b4f04be51bc212001d2c1ee3
Eric Kapalka <eric.kapalka@montana.edu>
2024-03-18 11:38:38 -0600

    Removed unnecessary imports

--- database_sqlite/models.py
+++ database_sqlite/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, DateTime, Boolean
+from sqlalchemy import Column, Integer, String, DateTime
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.sql import func
 
