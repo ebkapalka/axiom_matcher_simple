af2509a96ad939da7d79bbe772e86843cbefab7d
Eric Kapalka <eric.kapalka@montana.edu>
2024-04-12 17:02:44 -0600

    Did away with the "goto_verifier" function in navigate_verifier.py.  Instead, we're now building a URL to navigate directly to the correct verifier right here in axiom_driver.py.  Also changed some variable names to make more sense and added some validation at the beginning to check for an incorrect 'status' or 'environ'

--- browser_control/axiom_driver.py
+++ browser_control/axiom_driver.py
@@ -2,6 +2,7 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.common.by import By
 from selenium import webdriver
+import time
 import sys
 
 from browser_utilities.case_handlers import (handle_normal, handle_bad_ceeb, handle_bad_address,
@@ -11,7 +12,6 @@ from browser_utilities.navigate_verifier import identify_page, goto_verifier
 from matching_utilities.match_handler import handle_match_dialogue
 from browser_utilities.await_loading import wait_for_loading
 from database_sqlite.database import DatabaseManager
-
 
 HANDLERS = {
     "match dialogue": handle_match_dialogue,
@@ -32,22 +32,28 @@ class AxiomDriver:
     """
     Class to control the Axiom driver
     """
-    def __init__(self, database: DatabaseManager, run_mode: str = "test", option: str = "default"):
-        if run_mode == "test":
-            self.base_url = "https://axiom-elite-test.msu.montana.edu/"
-        elif run_mode == "prod":
-            self.base_url = "https://axiom-elite-prod.msu.montana.edu/"
-        else:
-            print("Invalid run mode")
+
+    def __init__(self, database: DatabaseManager, environ="test", source=601, status="Verify"):
+        """
+        Initialize the Axiom driver
+        :param database: DatabaseManager instance
+        :param environ: "prod" or "test"
+        :param status: "Verify" or "Error"
+        :param source: numeric key for the source
+        """
+        if environ not in ["test", "prod"]:
+            print("Invalid run env")
+            sys.exit()
+        if status not in ["Verify", "Error"]:
+            print("Invalid status")
             sys.exit()
 
+        self.base_url = f"https://axiom-elite-{environ}.msu.montana.edu"
+        self.verifier_url = (f"{self.base_url}/Verification/Verification.aspx"
+                             f"?SourceID={source}&Status={status}")
         self.driver = webdriver.Chrome()
         self.database = database
-        self.option = option
         self.await_login()
-        goto_verifier(self.driver,
-                      self.base_url,
-                      self.option)
         self.main_loop()
 
     def await_login(self):
@@ -57,7 +63,8 @@ class AxiomDriver:
         """
         print("Please log into Axiom")
         self.driver.get(self.base_url)
-        WebDriverWait(self.driver, 300).until(EC.title_is("Axiom Elite - Dashboard"))
+        WebDriverWait(self.driver, 300).until(
+            EC.title_is("Axiom Elite - Dashboard"))
         print("Axiom Login Successful")
 
     def main_loop(self):
@@ -65,6 +72,7 @@ class AxiomDriver:
         Main logic loop
         :return: None
         """
+        self.driver.get(self.verifier_url)
         while True:
             wait_for_loading(self.driver)
             WebDriverWait(self.driver, 30).until(
