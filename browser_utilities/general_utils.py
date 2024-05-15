6d3cf4227fbe8453518da6d81333b135160cd3e6
Eric Kapalka <eric.kapalka@montana.edu>
2024-05-15 16:06:18 -0600

    Added os.environ check for the username and password so I don't have to enter it so many times.  If the environment variable for it doesn't exist, the user will still be prompted.

--- browser_utilities/general_utils.py
+++ browser_utilities/general_utils.py
@@ -1,12 +1,6 @@
 import pwinput
 import sys
-
-
-REC_TYPES = {
-    "prospects": 601,
-    "act": 361,
-    # add other record types here as needed
-}
+import os
 
 
 def generate_urls(config: dict) -> tuple[str, str, str]:
@@ -16,18 +10,19 @@ def generate_urls(config: dict) -> tuple[str, str, str]:
     :return: tuple of base URL and login URL
     """
     run_mode = config["environment mode"]
-    rec_type = config["record type"]
     issue_types = set(config["issue types"])
+
+    # validate the configuration
     if run_mode not in ["test", "prod"]:
         print("Invalid run mode")
         sys.exit()
     if not issue_types.issubset({"verify", "error"}):
         print("Invalid issue type")
         sys.exit()
-    if rec_type not in REC_TYPES:
-        print("Invalid record type")
-        sys.exit()
-    source_id = REC_TYPES[config["record type"]]
+    # add more here as needed
+
+    # generate the URLs used by the fetcher and worker
+    source_id = config["source id"]
     manager_url = (f"https://axiom-elite-{run_mode}.msu.montana.edu"
                    f"/RecordManager.aspx?SourceID={source_id}")
     verifier_url = (f"https://axiom-elite-{run_mode}.msu.montana.edu"
@@ -37,12 +32,15 @@ def generate_urls(config: dict) -> tuple[str, str, str]:
     return manager_url, verifier_url, login_url
 
 
-def prompt_credentials() -> tuple[str, str]:
+def prompt_credentials(env="PROD") -> tuple[str, str]:
     """
     Prompt the user for their Axiom credentials
     :return: tuple of username and password
     """
-    username = input("Enter your Axiom username: ").strip()
-    password = pwinput.pwinput(prompt="Enter your Axiom password: ",
-                               mask='*').strip()
+    username = os.environ.get(f"AXIOM_{env}_USERNAME")
+    password = os.environ.get(f"AXIOM_{env}_PASSWORD")
+    if not username or not password:
+        username = input("Enter your Axiom username: ").strip()
+        password = pwinput.pwinput(prompt="Enter your Axiom password: ",
+                                   mask='*').strip()
     return username, password
