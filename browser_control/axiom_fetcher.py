ea4d1dda9de0ab5e608ce826c8b7ce095bec7527
Eric Kapalka <eric.kapalka@montana.edu>
2024-05-15 16:08:28 -0600

    MAJOR changes to accommodate the changes made in Axiom Prod -- all different selectors / locators, fetching the record IDs with Javascript instead of other means (because EVERY "record id" has the same HTML ID, making this much faster and easier)

--- browser_control/axiom_fetcher.py
+++ browser_control/axiom_fetcher.py
@@ -1,16 +1,15 @@
-from selenium.webdriver import ActionChains
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
+from selenium.common import TimeoutException, ElementClickInterceptedException
+from selenium.webdriver import ActionChains
 from selenium.webdriver.common.by import By
 from selenium import webdriver
 import time
 
+from browser_utilities.navigate_verifier import perform_login
 from browser_utilities.await_loading import wait_for_loading
 from browser_utilities.general_utils import generate_urls
-from browser_utilities.navigate_verifier import perform_login
 from database_sqlite.database import DatabaseManager
-
-
 
 
 class AxiomFetcher:
@@ -29,46 +28,76 @@ class AxiomFetcher:
         self.goto_record_manager()
         self.main_loop()
 
-    def goto_record_manager(self):
+    def goto_record_manager(self, timeout=30):
+        """
+        Navigate to the record manager page
+        :return: None
+        """
         self.driver.get(self.manager_url)
+        selector_advanced_options = (By.XPATH, "//button[@aria-label='Toggle Advanced Search Options']")
+        selector_status_text = (By.XPATH, "//div[text()='Status...']")
+        selector_status_input = (By.XPATH, "//input[@aria-label='Select Statuses']")
+        selector_search_button = (By.XPATH, "//button[@aria-label='Search Record Manager']")
+
         # make the advanced options visible
-        input_status = WebDriverWait(self.driver, 5).until(
-            EC.presence_of_element_located((By.ID, "react-select-3-input")))
-        while not input_status.is_displayed():
-            button_options = WebDriverWait(self.driver, 5).until(
-                EC.element_to_be_clickable((By.ID, "ToggleAdvancedOptions")))
-            button_options.click()
-            time.sleep(.1)
+        while True:
+            button_options = WebDriverWait(self.driver, 1).until(
+                EC.element_to_be_clickable(selector_advanced_options))
+            try:
+                WebDriverWait(self.driver, 1).until(
+                    EC.presence_of_element_located(selector_status_text))
+                break
+            except TimeoutException:
+                button_options.click()
+                time.sleep(0.5)
+                continue
+            except Exception as e:
+                print("Goto record manager error:", type(e))
+                button_options.click()
+                time.sleep(0.5)
+                continue
 
         # set the status options
+        input_status = WebDriverWait(self.driver, timeout).until(
+            EC.presence_of_element_located(selector_status_input))
         for status in self.options:
             input_status.send_keys(f"{status}\n")
 
         # click the search button
         WebDriverWait(self.driver, 5).until(
-            EC.element_to_be_clickable((By.ID, "SearchButton"))).click()
+            EC.element_to_be_clickable(selector_search_button)).click()
 
     def main_loop(self):
         """
         Main logic loop
         :return: None
         """
+        fetch_script = """
+            return Array.from(
+                document.querySelectorAll('#record-id')
+            ).map(element => element.textContent);
+        """
         while True:
             wait_for_loading(self.driver)
             page_urls = []
-            rows = WebDriverWait(self.driver, 5).until(
-                EC.presence_of_all_elements_located((By.CLASS_NAME, "rdt_TableRow")))
-            for row in rows:
-                cols = row.find_elements(By.CLASS_NAME, "rdt_TableCell")
-                page_urls.append(f"{self.verifier_url}{cols[2].text}")
+            WebDriverWait(self.driver, 30).until(
+                EC.presence_of_element_located((By.ID, "record-id")))
+            prospect_ids = self.driver.execute_script(fetch_script)
+            for prospect_id in prospect_ids:
+                new_url = f"{self.verifier_url}{prospect_id}"
+                print(new_url); page_urls.append(new_url)
             self.database.bulk_add_urls(page_urls)
 
-            nav_box = WebDriverWait(self.driver, 5).until(
-                EC.presence_of_element_located((By.CLASS_NAME, "rdt_Pagination")))
-            button_next = nav_box.find_element(By.ID, "pagination-next-page")
-            span_stats = nav_box.find_element(By.XPATH, ".//span[1]").text
+            # navigate to the next page
+            selector = (By.XPATH, "//button[@title='Go to the next page']")
+            button_next = WebDriverWait(self.driver, 5).until(
+                EC.presence_of_element_located(selector))
             self.actions.move_to_element(button_next).perform()
-            WebDriverWait(self.driver, 5).until(
-                EC.element_to_be_clickable((By.ID, "pagination-next-page")))
-            # print(span_stats)
-            button_next.click()
+            while True:
+                try:
+                    button_next = WebDriverWait(self.driver, 5).until(
+                        EC.element_to_be_clickable(selector))
+                    button_next.click()
+                    break
+                except ElementClickInterceptedException:
+                    time.sleep(0.5)
